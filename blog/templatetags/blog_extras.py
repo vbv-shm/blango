fa5b7d07386618c9e90acf8442f7997bf4927098
Vaibhav Sharma <nobodies.email.id@gmail.com>
2025-07-19 13:07:40 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,8 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
+
 register=template.Library()
 user_model=get_user_model()
 
@@ -27,3 +29,25 @@ def author_details(author,user):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">',extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col" {}', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html('</div>')
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
